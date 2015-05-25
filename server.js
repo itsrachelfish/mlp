0a8ec079f00f94c8afda9485ceddc8bbfb0cc201
Rachel Fish <itsrachelfish@gmail.com>
2015-05-25 17:48:19 -0600

    Setup wetfish server example

---
+++ server.js
@@ -0,0 +1,9 @@
+// Start the server
+var config = require('./config');
+var server = require('wetfish-server').createServer(config);
+
+// Add a custom model
+require('./models/example')(server.model);
+
+// Add some routes
+require('./routes/pages')(server);

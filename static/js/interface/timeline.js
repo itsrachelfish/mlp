2161bc6b1e44bd8f31d2026eaae5f00dd53ecda1
Rachel Fish <itsrachelfish@gmail.com>
2015-08-02 01:04:34 -0600

    Code for adding layers with dynamic timelines

--- static/js/interface/timeline.js
+++ static/js/interface/timeline.js
@@ -1,8 +1,75 @@
 // Code for the timeline
 var Menu = require('../plugins/menu');
 
+var timeline =
+{
+    // Function for drawing the tick marks on a canvas
+    drawTick: function(context, start, length)
+    {
+        context.beginPath();
+        context.moveTo(start[0], start[1]);
+        context.lineTo(start[0], start[1] + length);
+        context.stroke();
+    },
+    
+    // Function to generate the ticks on each layer
+    drawTicks: function(layer, canvas, duration)
+    {
+        // Make sure the canvas fits into the available space
+        var size = $(layer).size();
+
+        // Remove the border size
+        size.width -= parseFloat($(layer).style('border-left-width')) + parseFloat($(layer).style('border-right-width'));
+        size.height -= parseFloat($(layer).style('border-top-width')) + parseFloat($(layer).style('border-bottom-width'));
+        
+        $(canvas).attr('width', size.width);
+        $(canvas).attr('height', size.height);
+
+        // Clear the canvas and draw new ticks
+        var context = canvas.getContext('2d');
+
+        // Make sure the canvas is cleared
+        context.clearRect(0, 0, size.width, size.height);
+
+        for(var i = 0, l = duration + 1; i < l; i++)
+        {
+            var offset = (size.width / duration) * i;
+            
+            if(i % 10)
+            {
+                timeline.drawTick(context, [offset, size.height], -5);
+            }
+            else
+            {
+                timeline.drawTick(context, [offset, size.height], -10);
+            }
+        }
+    }
+};
+
 $(document).ready(function()
 {
     var menu = new Menu({title: 'Timeline', selector: '.templates .timeline', class: 'timeline-menu'});
     $('.timeline-menu').dragondrop({handle: '.title'});
+
+    $('.timeline .add-layer').on('click', function()
+    {
+        // Add the new layer to the layers container
+        var layer = $('.templates .layer-wrap').clone();
+        $('.timeline .layers').el[0].appendChild(layer);
+
+        // Draw tick marks on the layer's canvas
+        var duration = $('.timeline .duration').value();
+        timeline.drawTicks($(layer).find('.layer').el[0], $(layer).find('canvas').el[0], duration);
+    });
+
+    $('.timeline .duration').on('input', function()
+    {
+        // Update tick marks on each layer whenever the duration is changed
+        $('.timeline .layer').each(function()
+        {
+            var duration = $('.timeline .duration').value();
+            timeline.drawTicks(this, $(this).find('canvas').el[0], duration);
+        });
+    });
 });

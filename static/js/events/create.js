8d6a9deddac06bccfc2e94ab0335275d73e70462
Rachel Fish <itsrachelfish@gmail.com>
2015-08-28 23:57:35 -0600

    JS to show/hide presets and close create menu

--- static/js/events/create.js
+++ static/js/events/create.js
@@ -3,4 +3,27 @@ var Menu = require('../plugins/menu');
 $(document).ready(function()
 {
     var menu = new Menu({title: 'Create Object', selector: '.templates .create', class: 'create-menu', state: 'closed'});
+
+    // Make sure nothing is selected by default
+    $('.create .type option').eq(0).prop('selected', true);
+
+    // Show / hide the preset options when necessary
+    $('.create .type').on('change', function()
+    {
+        var type = $(this).value().toLowerCase();
+
+        if(type == 'preset')
+        {
+            $('.create .presets').style({display: 'block'});
+        }
+        else
+        {
+            $('.create .presets').style({display: 'none'});
+        }
+    });
+
+    $('.create .cancel').on('click', function()
+    {
+        menu.close();
+    });
 });
